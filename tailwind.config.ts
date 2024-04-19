0351c17de19077e99287af62f899633f75b8500d
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-04-19 21:34:39 +0200

    Gradient
    Gradient colors in tailwind config and usage of them

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"gradient-from": "rgba(255, 130, 188, 1)",
+				"gradient-via": "rgba(130, 217, 255, 1)",
+				"gradient-to": "rgba(61, 163, 178, 1)",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
