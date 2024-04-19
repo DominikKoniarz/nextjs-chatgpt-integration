5aed9cc55992a28145207d58ec17dec60f3160db
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-04-19 22:37:29 +0200

    Added blank chat page

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+	return (
+		<main className="from-gradient-from via-gradient-via to-gradient-to flex h-full w-full flex-col bg-gradient-to-tr via-[52%] text-white">
+			Chat page
+		</main>
+	);
+}
